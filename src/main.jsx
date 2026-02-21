d3ad69178b4b932caae6ab7d43b24cade2a28427
https://github.com/Shreejad123 <shreejadkotian00@gmail.com>
2026-02-21 07:05:47 +0530

    Changed the file name

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Registration from "./components/registrationForm";
 import LoginForm from "./components/Login";
 import ForgotPassword from "./components/ForgotPassword";
-import AddSurgery from "./components/addSurgery";
+import AddSurgery from "./components/addSurgeries";
 import Dashboard from "./components/dashboard";
 import SurgeryTable from "./components/surgeryTable";
-import EditSurgery from "./components/editSurgery";
+import EditSurgery from "./components/editSurgeries";
 import PatientDetails from "./components/patientDetails";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
